7c5a229740c2c810415288a406c8e2ee6a1d1981
Ryan Dabler <rpdabler@gmail.com>
2018-10-30 19:43:59 -0600

    Updates

---
+++ precache-manifest.99ef56d171a1ffd6b49b5f915e59a8e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/idlelife/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1e45c793814ecc120001",
+    "url": "/idlelife/static/js/runtime~main.1e45c793.js"
+  },
+  {
+    "revision": "e18f62dd5fb2ec598f1b",
+    "url": "/idlelife/static/js/main.e18f62dd.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/idlelife/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "e18f62dd5fb2ec598f1b",
+    "url": "/idlelife/static/css/main.f89586c1.chunk.css"
+  },
+  {
+    "revision": "48dc859982f9369119f53c5c6b96dc45",
+    "url": "/idlelife/index.html"
+  }
+];
