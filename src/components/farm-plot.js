14705cf83e9f978232b767a3c13eea390aa7dd8f
Ryan Dabler <rpdabler@gmail.com>
2018-11-10 13:42:45 -0700

    change selectedSlot in store on click

--- src/components/farm-plot.js
+++ src/components/farm-plot.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import STORE from '../stores/store';
 
 class FarmPlot extends Component {
+    constructor(props) {
+        super(props);
+        this.onClick = this.onClick.bind( this );
+    }
+
+    onClick() {
+        const { slotNum } = this.props;
+        STORE.locations['farm'].selectedSlot = slotNum;
+    }
+
     render() {
+        const { slotNum } = this.props;
+        const { locations: { farm: { selectedSlot } } } = STORE;
         return (
-            <div className="farm-plot" style={{backgroundColor: 'white'}}>
+            <div className={`farm-plot ${slotNum === selectedSlot ? 'selected' : ''}`} style={{backgroundColor: 'white'}} onClick={this.onClick}>
                 PLOTS
             </div>
         );
