74a2bd942889b4d64fac975db126c2397a745836
Ryan Dabler <rpdabler@gmail.com>
2018-11-10 13:36:00 -0700

    display farm plots

--- src/components/panels/garden-panel.js
+++ src/components/panels/garden-panel.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
+import FarmPlot from '../farm-plot';
+import STORE from '../../stores/store';
 
 class GardenPanel extends Component {
     static displayName = 'GardenPanel';
 
     render() {
+        const { currentLocation } = STORE;
+        const { slots } = STORE.locations[currentLocation];
+        const plots = Array(slots).fill(0);
+
         return (
             <section className="garden-panel" style={{gridArea: 'b', backgroundColor: 'red'}}>
-                GARDEN PANEL
+                { plots.map((plot, idx) => <FarmPlot key={idx} slotNum={idx} />) }
             </section>
         );
     }
