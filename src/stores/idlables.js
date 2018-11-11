ae10aa2f555b606c2303ddeba1da821c881de0b3
Ryan Dabler <rpdabler@gmail.com>
2018-11-11 12:37:24 -0700

    make idlables time be in ms

--- src/stores/idlables.js
+++ src/stores/idlables.js
@@ -3,19 +3,19 @@ const IDLABLES = [
         id: 1,
         itemId: 1,
         name: 'Carrot',
-        time: 5
+        time: 5000
     },
     {
         id: 2,
         itemId: 2,
         name: 'Cucumber',
-        time: 10
+        time: 10000
     },
     {
         id: 3,
         itemId: 3,
         name: 'Cabbage',
-        time: 15
+        time: 15000
     }
 ];
 
