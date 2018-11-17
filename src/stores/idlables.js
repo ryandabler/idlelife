8225e9a540751e925fe93b6192484f9952ed1ffb
Ryan Dabler <rpdabler@gmail.com>
2018-11-17 13:24:20 -0700

    add unloaded flag to idlables

--- src/stores/idlables.js
+++ src/stores/idlables.js
@@ -3,19 +3,22 @@ const IDLABLES = [
         id: 1,
         itemId: 1,
         name: 'Carrot',
-        time: 5000
+        time: 5000,
+        unlocked: true
     },
     {
         id: 2,
         itemId: 2,
         name: 'Cucumber',
-        time: 10000
+        time: 10000,
+        unlocked: false
     },
     {
         id: 3,
         itemId: 3,
         name: 'Cabbage',
-        time: 15000
+        time: 15000,
+        unlocked: false
     }
 ];
 
