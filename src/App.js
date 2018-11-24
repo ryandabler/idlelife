2a87fabfb595c1db8d22fed00129d87f4e4aadfa
Ryan Dabler <rpdabler@gmail.com>
2018-11-24 13:04:36 -0700

    add initial stages of routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from './components/header';
 import GardenPanel from './components/panels/garden-panel';
 import GermanPanel from './components/panels/german-panel';
@@ -8,13 +9,19 @@ import IdlePanel from './components/panels/idle-panel';
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<Header />
-				<IdlePanel />
-				<GardenPanel />
-				<TextPanel />
-				<GermanPanel />
-			</div>
+			<Router>
+				<div className="App">
+					<Header />
+					<Switch>
+						<Route exact path="/" render={() => ([
+							<IdlePanel key="idlepanel" />,
+							<GardenPanel key="gardenpanel" />,
+							<TextPanel key="textpanel" />,
+							<GermanPanel key="germanpanel" />
+						])} />
+					</Switch>
+				</div>
+			</Router>
 		);
 	}
 }
