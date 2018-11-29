322e0dc6a80a4bb1bb4b85c01f12f4c2f9cf4ac5
Ryan Dabler <rpdabler@gmail.com>
2018-11-28 18:37:55 -0700

    hook into live inventory list from store

--- src/components/panels/player-panel.js
+++ src/components/panels/player-panel.js
@@ -1,39 +1,8 @@
 import React, { Component } from 'react';
 import ListContainer from '../list-container';
 import StatItem from '../list-items/stat-item';
+import STORE from '../../stores';
 
-const ITEMS = [
-    {
-        id: 1,
-        name: 'Carrot',
-        quantity: 15
-    },
-    {
-        id: 2,
-        name: 'Cucumber',
-        quantity: 10
-    },
-    {
-        id: 3,
-        name: 'Cabbage',
-        quantity: 5
-    },
-    {
-        id: 4,
-        name: 'Broccoli',
-        quantity: 8
-    },
-    {
-        id: 5,
-        name: 'Cauliflower',
-        quantity: 23
-    },
-    {
-        id: 6,
-        name: 'Rutabaga',
-        quantity: 17
-    }
-];
 class PlayerPanel extends Component {
     static displayName = 'PlayerPanel';
 
@@ -41,10 +10,10 @@ class PlayerPanel extends Component {
         return (
             <section className="player-panel" style={{gridArea: 'b'}}>
                 <h2>Inventory</h2>
-                <ListContainer items={ITEMS} onItemSelect={() => {}} itemComponent={StatItem} />
+                <ListContainer items={STORE.inventoryList} onItemSelect={() => {}} itemComponent={StatItem} />
                 <h2>Statistics</h2>
                 <h3>Idling</h3>
-                <ListContainer items={ITEMS} onItemSelect={() => {}} itemComponent={StatItem} />
+                <ListContainer items={[]} onItemSelect={() => {}} itemComponent={StatItem} />
             </section>
         );
     }
