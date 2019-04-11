ec52f42b29dc7ce10b05e99ee6489e51faffba74
Ryan Dabler <rpdabler@gmail.com>
2019-04-10 20:18:57 -0600

    begin fleshing out world building

--- src/components/world-builder.js
+++ src/components/world-builder.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
+import STORE from '../stores/store';
+import { buildMaps } from '../utilities/world-gen';
+import { addToDatabase } from '../utilities/indexeddb';
 
 class WorldBuilder extends Component {
     static displayName = 'WorldBuilder';
 
+    generateWorld() {
+        const userName = STORE.player.name;
+
+        const maps = buildMaps();
+        Object.entries(maps).forEach( async ([ map, mapData ]) => {
+            const object = {
+                userName,
+                map,
+                data: mapData
+            }
+            addToDatabase('maps', object);
+        });
+        
+    }
+
     render() {
+        this.generateWorld(); // The synchonicity here prevents the loader from showing until AFTER the world has been generated
+
         return (
             <div>
                 Loading
